0e5262652a27cd8ad019f056c3505fd3495e2099
naelsen <nael.sennoun@gmail.com>
2020-03-14 17:35:52 +0100

    modif

--- projet.c
+++ projet.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 
-
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -11,24 +10,28 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-void affichage(char c,int a, int b){
 
-	printf("_________________________\n");
-	for(int i=0; i<10 ;i++){
-		for(int j =0; j<8 ; j++){
-			if (i==a && j==b){printf(ANSI_COLOR_BLUE " %c ",c);}
+void affichage(char c, int x, int y)
+{
+	printf("\n");
+	for(int i=1; i<=6 ;i++)
+	{
+		printf("|");
+		for(int j = 1; j<= 7 ; j++)
+		{
+			if (i == x && j == y)
+				printf(ANSI_COLOR_BLUE " %c ",c);
 			else
 				printf(ANSI_COLOR_GREEN " %c ",'.');
 		}
 		printf(ANSI_COLOR_RESET"|\n");
 	}
-	printf("_________________________\n");
+	printf("\n");
 }
 
 int main()
 {
-
-	affichage('o',8,2);
-	affichage('x',6,5);
-	
+	affichage('o',6,1);
+	affichage('o',6,2);
+	return 0;
 }
