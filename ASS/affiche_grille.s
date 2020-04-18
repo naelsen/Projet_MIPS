cd47d68992066b1ed5708b1193e71315b2775589
Aissam08 <rabhi.aissam@hotmail.fr>
2020-04-18 15:33:34 +0200

    affichage

--- ASS/affiche_grille.s
+++ ASS/affiche_grille.s
@@ -1,8 +1,8 @@
 #Permet d'afficher la grille#
 .data
-tab : .asciiz "   |"
+tab : .asciiz "|   "
 tab2 : .asciiz "+---"
-jump : .asciiz "\n"
+jump : .asciiz "|\n"
 
 .text
 main :  
@@ -17,13 +17,30 @@ ligne:
         	syscall
         	j colonne
 	saut :
-	    bge $t4,6, end
+	    bge $t4,12, end
 	    li $v0 4
 	    la $a0, jump
 	    addi $t4, $t4,1
 	    li $t5 0
             syscall
-            j while
+            j re
+            
+        re : 
+        	bge $t5,7,saut2
+     		addi $t5,$t5,1
+    		li   $v0 4
+        	la $a0, tab2
+        	syscall
+        	j re
+        	
+       saut2 :
+	    bge $t4,12, end
+	    li $v0 4
+	    la $a0, jump
+	    addi $t4, $t4,1
+	    li $t5 0
+            syscall
+            j colonne
           
        end :
        		li $v0 10
