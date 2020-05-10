d4c223b52920c7f27c900b6dd8f1b338a3cfcbfd
naelsen <nael.sennoun@gmail.com>
2020-05-10 17:11:55 +0200

    Ca a marcher la

--- inshallahCaMarche.asm
+++ inshallahCaMarche.asm
@@ -3,7 +3,9 @@
 ligneH:		.asciiz "\n+---+---+---+---+---+---+---+\n"
 numColonne:	.asciiz "  1   2   3   4   5   6   7  \n"
 saut:		.asciiz "\n"
-
+erreur:		.asciiz "Choisissez un nombre en 1 et 7\n"
+joueur1:	.asciiz "\nAu tour du joueur 1 : "
+joueur2:	.asciiz "\nAu tour du joueur 2 : "
 # Dans la grille :
 # 0 = Rien
 # 1 = J1
@@ -30,10 +32,15 @@ main:
 	sb $s0, 0($a1)                          # On aurait pu faire aussi sb $s0, 35($a1) au lieu des 3 etapes.
 	addi $a1, $a1, -35			# Mais on peut pas faire ca dans la boucle donc je reste cohérent
 	
-	jal FCT_PRINT
-	#jal JouerJ1
-	#jal FCT_PRINT
-	#jal JouerJ2
+	TOUR_J1:
+		jal FCT_PRINT
+		jal FCT_JouerJ1
+		j TOUR_J2
+	
+	TOUR_J2:
+		jal FCT_PRINT
+		jal FCT_JouerJ2
+		j TOUR_J1
 
 	li $v0, 10
 	syscall
@@ -45,48 +52,47 @@ FCT_PRINT:
 	sw $fp, 4($sp)    #     OG
 	addi $fp, $sp, 8  #       UE
 	
-	addi $t2, $t2, 0                                  #t2 Compteur ligne
+	addi $t2, $0, 0                                  #t2 Compteur ligne
 	PRINT:
-	la $a0, ligneH
-	li $v0, 4
-	syscall
-	addi $t2, $t2, 1
-	beq $t2, 7, FIN_PRINT
-	addi $t0, $0, -1   # compteur de la grille         #t0 Compteur colonne
-	PRINT_LIGNE:
-		beq $t0, 6, FIN_PRINT_LIGNE
-		addi $t0, $t0, 1
-		lb $t1, 0($a1)                                #t1 Valeur de la case
-		
-		AFFICHE_CASE:
-			beq $t1, 0, AFFICHE_VIDE
-			j TEST_J1
-			AFFICHE_VIDE:
-			la $a0, videa                       # Affiche `"|   " si case de grille stockée dans t1 egale 0
-			li $v0, 4
-			syscall
-			j CASE_AFFICHE
+		la $a0, ligneH
+		li $v0, 4
+		syscall
+		addi $t2, $t2, 1
+		beq $t2, 7, FIN_PRINT            # On verifie si on peut encore print une ligne
+		addi $t0, $0, -1   # compteur de la grille         #t0 Compteur colonne
+		PRINT_LIGNE:
+			beq $t0, 6, FIN_PRINT_LIGNE         # On verifie si on peut encore print une case de la ligne
+			addi $t0, $t0, 1
+			lb $t1, 0($a1)                                #t1 Valeur de la case
 			
-			TEST_J1:
-			beq $t1, 1, AFFICHE_J1
-			j AFFICHE_J2
-			AFFICHE_J1:
-			la $a0, J1a                       # Affiche `"| O " si case de grille stockée dans t1 egale 1
-			li $v0, 4
-			syscall
-			j CASE_AFFICHE
-			
-			
-			AFFICHE_J2:
-			la $a0, J2a                     # Affiche `"| X "  si case de grille stockée dans t1 egale 2
-			li $v0, 4
-			syscall
-			j CASE_AFFICHE
-		CASE_AFFICHE:
-		addi $a1, $a1, 1                             # a1 + 1      (7 fois dans cette boucle qui est elle meme dans la boucle de 6)
-	j PRINT_LIGNE
-	FIN_PRINT_LIGNE:
-	j PRINT
+			AFFICHE_CASE:
+				beq $t1, 0, AFFICHE_VIDE
+				j TEST_J1
+				AFFICHE_VIDE:
+				la $a0, videa                       # Affiche `"|   " si case de grille stockée dans t1 egale 0
+				li $v0, 4
+				syscall
+				j CASE_AFFICHE
+				
+				TEST_J1:
+				beq $t1, 1, AFFICHE_J1
+				j AFFICHE_J2
+				AFFICHE_J1:
+				la $a0, J1a                       # Affiche `"| O " si case de grille stockée dans t1 egale 1
+				li $v0, 4
+				syscall
+				j CASE_AFFICHE
+
+				AFFICHE_J2:
+				la $a0, J2a                     # Affiche `"| X "  si case de grille stockée dans t1 egale 2
+				li $v0, 4
+				syscall
+				j CASE_AFFICHE
+			CASE_AFFICHE:
+			addi $a1, $a1, 1                             # a1 + 1      (7 fois dans cette boucle qui est elle meme dans la boucle de 6)
+			j PRINT_LIGNE
+		FIN_PRINT_LIGNE:
+		j PRINT
 	FIN_PRINT:
 	la $a0, numColonne
 	li $v0, 4
@@ -98,9 +104,75 @@ FCT_PRINT:
 	jr $ra           #       UE
 	
 	
+
+FCT_JouerJ1:
+	addi $sp, $sp, -8 # PR
+	sw $ra, 0($sp)    #   OL
+	sw $fp, 4($sp)    #     OG
+	addi $fp, $sp, 8  #       UE
+	LIRE_J1:
+		la $a0, joueur1
+		li $v0, 4
+		syscall
+		li $v0, 5
+		syscall
+		addi $t1,$v0, -1               # scanf t1 (on fait -1 pour collé a la case de la grille qui est de 0 à 41)
+		addi $t3, $0, -1
+		bgt $t1, $t3, verif_J1		#
+		la $a0, erreur			#
+		li $v0, 4			#
+		syscall				#
+		j LIRE_J1			#
+		verif_J1:			# On verifie si le nombre est entre 1 et 7
+		addi $t2, $0, 7			#            t2 = 7
+		bgt $t2, $t1, FIN_LIRE_J1	#
+		la $a0, erreur			#
+		li $v0, 4			#
+		syscall				#
+		j LIRE_J1
+	FIN_LIRE_J1:
+	PLACER_J1:
+		
+	FIN_PLACER_J1:
+	lw $ra, 0($sp)   # EP
+	lw $fp, 4($sp)   #   IL
+	addi $sp, $sp, 8 #     OG
+	jr $ra           #       UE
 	
 	
-	
+FCT_JouerJ2:
+	addi $sp, $sp, -8 # PR
+	sw $ra, 0($sp)    #   OL
+	sw $fp, 4($sp)    #     OG
+	addi $fp, $sp, 8  #       UE
+	LIRE_J2:
+		la $a0, joueur2
+		li $v0, 4
+		syscall
+		li $v0, 5
+		syscall
+		addi $t1,$v0, -1               # scanf t1 (on fait -1 pour collé a la case de la grille qui est de 0 à 41)
+		
+		bgt $t1, $0, verif_J2		#
+		la $a0, erreur			#
+		li $v0, 4			#
+		syscall				#
+		j LIRE_J2			#
+		verif_J2:			# On verifie si le nombre est entre 1 et 7
+		addi $t2, $0, 8			#
+		bgt $t2, $t1, FIN_LIRE_J2	#
+		la $a0, erreur			#
+		li $v0, 4			#
+		syscall				#
+		j LIRE_J2
+	FIN_LIRE_J2:
+	PLACER_J2:
+		
+	FIN_PLACER_J2:
+	lw $ra, 0($sp)   # EP
+	lw $fp, 4($sp)   #   IL
+	addi $sp, $sp, 8 #     OG
+	jr $ra           #       UE
 	
 	
 	
